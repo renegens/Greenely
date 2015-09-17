123cd01c24cfad6f6d81fddbbd57168daeb844d5
renegens <renegens@gmail.com>
2015-09-17 14:36:37 +0300

    Initial Commit

---
+++ Greenely Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Greenely Test
+//
+//  Created by Giwrgos Gens on 17/09/15.
+//  Copyright © 2015 Rene Gens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
